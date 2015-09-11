00cece4f62f9ce99e1ec061c323ebb8c354bca96
SergerGood <serzher@inbox.ru>
2015-09-11 15:50:01 +0300

    what is this

---
+++ Source/Service.DataLayer/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using NHibernate;
+
+using Service.DataLayer.Entities;
+
+
+namespace Service.DataLayer
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void Save(Order order)
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(order);
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
