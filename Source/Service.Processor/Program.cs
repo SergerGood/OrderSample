2ab870b4e4fb76cfa61737e53874368b14ca4e1f
SergerGood <serzher@inbox.ru>
2015-09-11 11:15:48 +0300

    processor receive request

---
+++ Source/Service.Processor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+using Nelibur.Sword.Extensions;
+
+using Newtonsoft.Json;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using Service.Contracts.Requests;
+
+
+namespace Service.Processor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var model = connection.CreateModel())
+                {
+                    var consumer = new QueueingBasicConsumer(model);
+                    model.BasicConsume("request queue", false, consumer);
+
+                    while (true)
+                    {
+                        ReceiveMessage(model, consumer);
+                    }
+                }
+            }
+        }
+
+
+        private static void ReceiveMessage(IModel model, QueueingBasicConsumer consumer)
+        {
+            BasicDeliverEventArgs deliverEventArgs = consumer.Queue.Dequeue();
+
+            var request = deliverEventArgs.Body
+                .ToOption()
+                .Map(Encoding.UTF8.GetString)
+                .Map(JsonConvert.DeserializeObject<CreateOrderRequest>)
+                .Value;
+
+            model.BasicAck(deliverEventArgs.DeliveryTag, false);
+        }
+    }
+}
